c627f7ace0aedf2f9e256bc28c8011d45878dfae
Hayden Mak <haydenmlh@gmail.com>
2025-12-02 00:17:07 +0800

    Update camera

--- washed-away/Assets/Scripts/CameraFollowPlayer.cs
+++ washed-away/Assets/Scripts/CameraFollowPlayer.cs
@@ -14,6 +14,6 @@ public class CameraFollowPlayer : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        transform.position = player.transform.position + new Vector3(0, 1, -5);
+        transform.position = player.transform.position + new Vector3(0, 1, -2);
     }
 }
